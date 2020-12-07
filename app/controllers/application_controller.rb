a4e392a6f15a2234c00195ee4c9f05f35b7f6d59
iori-m-0801 <s88.myoken.iori@gmail.com>
2020-12-07 14:38:27 +0900

    ユーザー管理機能修正3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,5 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :kana_first, :kana_last, :birthday])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname, :first_name, :last_name, :kana_first, :kana_last, :birthday])
   end
 end
