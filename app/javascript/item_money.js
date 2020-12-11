64478852083e42ac85c3b7784d69e1d39e49aada
iori-m-0801 <s88.myoken.iori@gmail.com>
2020-12-11 10:34:03 +0900

    商品出品機能修正

--- app/javascript/item_money.js
+++ app/javascript/item_money.js
@@ -2,7 +2,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(priceInput.value * 1.1)
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1)
+
+    const profitNumber = document.getElementById("profit");
+    const value_reset = Math.floor(priceInput.value * 0.1)
+    profitNumber.innerHTML = Math.floor(priceInput.value - value_reset)
   })
 });
