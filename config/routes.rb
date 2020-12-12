90aafd734b442ac909fbcdf4fcf8482476d83706
iori-m-0801 <s88.myoken.iori@gmail.com>
2020-12-12 13:55:23 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   get 'items/new' => 'items_new_path'
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show, :edit]
 end
